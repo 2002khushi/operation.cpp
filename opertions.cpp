7042b3ea8063ebd07e9752212a9b3df171828dca
khushi chaudhary <142868729+2002khushi@users.noreply.github.com>
2024-04-14 01:46:21 +0530

    Add files via upload

---
+++ opertions.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a ,b,c;
+    a =45;
+    b =67;
+    c = a+b;
+    c = a-b;
+    c = a/b;
+    c = a*b;
+    cout<< c ;
+    return 0;
+}
